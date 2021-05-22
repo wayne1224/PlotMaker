7ad12dcf448a48ab7d7fb771bb03be04bd174be4
Binray Shen <binray.shen@gmail.com>
2021-05-22 21:38:13 +0800

    feat: 新增Tab3

--- MainWindow.py
+++ MainWindow.py
@@ -1,6 +1,7 @@
 import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
 from Tab2 import Tab2
+from Tab3 import Tab3
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
@@ -12,14 +13,14 @@ class MainWindow(QtWidgets.QMainWindow):
 
         #創建3個tab
         self.tab2 = Tab2()
+        self.tab3 = Tab3()
 
         #設定tab的css
         self.setStyleSheet( "QTabBar::tab { height: 40px; width: 250px; }")
   
         #將tab加入MainWindow中
         self.mainTab.addTab(self.tab2, "簡介與角色")
-
-    
+        self.mainTab.addTab(self.tab3, "劇情內容")
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
